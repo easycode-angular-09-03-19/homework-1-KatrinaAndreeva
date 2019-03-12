4487b72579eda893633dccfd3d7b410aa4b63faf
katrinaAndreeva <katya-andreeva@ukr.net>
2019-03-12 21:23:07 +0200

    added 2nd part

--- src/homework.ts
+++ src/homework.ts
@@ -31,3 +31,51 @@ interface Admin {
     password: string;
     type?: string;
 }
+//2.1. Создайте абстрактный класс Car с двумя методами drive (ехать) и refuel (заправка)
+// а также с двумя свойствами mileage (общий пробег машины) и fuel (количество бензина в машине).
+
+abstract class Car {
+    protected _mileage: number;
+   protected _fuel: number;
+
+    constructor(mileage: number, fuel: number) {
+        this._mileage = mileage;
+        this._fuel = fuel;
+    }
+    public abstract drive(km: number): void;
+    public abstract refuel(gas: number): void;
+}
+
+//2.2. Наследоваться от абстрактного класса Car и реализовать методы абстрактного класса drive
+// (ехать) и refuel (заправка). Метод drive должен принимать количество километров и обновлять
+// свойство mileage и уменьшать значение свойства fuel если бензин закончился то нужно вернуть
+// сообщение о том что нужно заправиться. Метод refuel должен увеличивать свойство fuel.
+// Обязательно делать проверку переданных данных. Свойства fuel и mileage должны быть protected.
+
+class NewCar extends Car{
+    constructor(_mileage: number, _fuel: number) {
+        super (_mileage, _fuel);
+    }
+    public drive(km: number): void {
+        if (km <= this._fuel) {
+            this._mileage += km;
+            this._fuel -= km;
+        }
+    }
+    public refuel(gas: number): void{
+        if (gas <= 0){
+            console.log('error');
+        }
+        if (gas > 0) {
+            this._fuel += gas;
+        }
+    }
+public get fuel(): number{
+    return this._fuel;
+}
+public get mileage(): number{
+    return this._mileage;
+}
+}
+
+//2.3.Создать публичный get для получения свойств fuel и mileage.
