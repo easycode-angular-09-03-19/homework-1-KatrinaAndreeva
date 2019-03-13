538d3c080a0f7395a56797d24d349f47f4b65eb6
katrinaAndreeva <katya-andreeva@ukr.net>
2019-03-13 21:05:30 +0200

    fixed mistakes

--- src/homework.ts
+++ src/homework.ts
@@ -7,15 +7,12 @@ console.log(fuctorial(7));
 //1.2. Создать функцию multiply, которая будет принимать любое количество чисел и 
 // возвращать их произведение: multiply(1,2,3) = 6 (1*2*3)
 //Если нет ни одного аргумента, вернуть ноль: multiply() // 0
-function multyply(...arguments: number[]): number {
-    if(arguments.length == 0) return 0;
-       let multyply = 1;
-       for (let num of arguments) {
-           multyply *= num;
-       }
-       return multyply;
-   }
-   const resOfMulty: number = multyply(1, 2, 3);
+   function multiply(...numbers: number[]): number {
+    if (numbers.length == 0) return 0;
+
+    return numbers.reduce((a, b) => a * b);
+}
+   const resOfMulty: number = multiply(1, 2, 3);
    console.log(resOfMulty);
 //1.3. Создать функцию, которая принимает строку и возвращает строку-перевертыш: 
 // reverseString(‘test’) // “tset”.
@@ -70,10 +67,10 @@ class NewCar extends Car{
             this._fuel += gas;
         }
     }
-public get fuel(): number{
+    public get fuel(): number{
     return this._fuel;
 }
-public get mileage(): number{
+    public get mileage(): number{
     return this._mileage;
 }
 }
